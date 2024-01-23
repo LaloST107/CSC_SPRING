9963f3fd02383a99f419b388d5a7cae99124c011
LaloST107 <114461313+LaloST107@users.noreply.github.com>
2024-01-23 12:12:40 -0600

    Se manda el Cotroller, DTO,Entity Mapper,MapperImpl, Repository, ServiceIpl de factura y se agrega el de user

--- clicksoft/src/main/java/centro_soluciones/clicksoft/controller/FacturaController.java
+++ clicksoft/src/main/java/centro_soluciones/clicksoft/controller/FacturaController.java
@@ -1,4 +1,38 @@
 package centro_soluciones.clicksoft.controller;
 
+import centro_soluciones.clicksoft.dto.FacturaDTO;
+import centro_soluciones.clicksoft.service.FacturaService;
+import centro_soluciones.clicksoft.service.exception.ServiceException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/facturas")
 public class FacturaController {
+
+ private final FacturaService facturaService;
+
+    public FacturaController(FacturaService facturaService) {
+        this.facturaService = facturaService;
+    }
+
+    @GetMapping
+    public List<FacturaDTO> findAll() throws ServiceException {
+        return facturaService.findAll();
+    }
+
+    @PostMapping("/save")
+    public FacturaDTO save(@RequestBody FacturaDTO facturaDTO) throws ServiceException {
+
+        return facturaService.save(facturaDTO);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Integer id) throws  ServiceException{
+        facturaService.delete(id);
+    }
+
+
 }
--- clicksoft/src/main/java/centro_soluciones/clicksoft/service/Impl/FacturaServiceImpl.java
+++ clicksoft/src/main/java/centro_soluciones/clicksoft/service/Impl/FacturaServiceImpl.java
@@ -2,26 +2,46 @@ package centro_soluciones.clicksoft.service.Impl;
 
 
 import centro_soluciones.clicksoft.dto.FacturaDTO;
+import centro_soluciones.clicksoft.entity.FacturaEntity;
+import centro_soluciones.clicksoft.mappers.FacturaMapper;
 import centro_soluciones.clicksoft.repository.FacturaRepository;
 import centro_soluciones.clicksoft.service.FacturaService;
 import centro_soluciones.clicksoft.service.exception.ServiceException;
 import org.springframework.stereotype.Service;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class FacturaServiceImpl implements FacturaService {
-
+//  Hacemos la inyección por constructor enel cual le mandamos el repository y el factura mapper que tiene nuestros mapeos
     private final FacturaRepository facturaRepository;
-
-    public FacturaServiceImpl(final FacturaRepository facturaRepository) {
-        this.facturaRepository =  facturaRepository;
+    private final FacturaMapper facturaMapper;
+//En esta parte tambien le mandamos ese facturaMapper
+    public FacturaServiceImpl(final FacturaRepository facturaRepository, final FacturaMapper facturaMapper) {
+        this.facturaRepository = facturaRepository;
+        this.facturaMapper = facturaMapper;
     }
 
+
     @Override
+//   Metodo generado por el GenericService
     public List<FacturaDTO> findAll() throws ServiceException {
-        return null;
+        try {
+//           Almacenamos nuestra lista y ocupamos la QUERY de repository que se llama findAllCustom para hacer nuestro filro
+            List<FacturaEntity> lstEntity= facturaRepository.findAllCustom();
+//           Hacemos una lista para los DTO
+            List<FacturaDTO> lstDTO=new ArrayList<>();
+//          Hacemos la lista temporal y le agreagmos el facturaMapperc con tu ClinteEntity que lo pasa a DTO
+            for (FacturaEntity clienteEntity : lstEntity) {
+                lstDTO.add(facturaMapper.toDto(clienteEntity));
+            }
+//            Retornamos lista del dto
+            return lstDTO;
+        } catch (Exception e) {
+            throw new ServiceException(e);
+        }
+
     }
 
     @Override
@@ -30,9 +50,23 @@ public class FacturaServiceImpl implements FacturaService {
     }
 
     @Override
+    //   Metodo generado por el GenericService
     public FacturaDTO save(FacturaDTO facturaDTO) throws ServiceException {
-        return null;
+        try {
+//  Hacemos el maepeo del DTO a un entity y lo guardamos
+            FacturaEntity facturaEntity = facturaMapper.toEntity(facturaDTO);
+// Al tener ya nuestro mapeo lo guardamos con el save en el repository
+            FacturaEntity facturaSave = facturaRepository.save(facturaEntity);
+// Creamos este mapeo para mostrarlo al momento de hacer el registro
+            FacturaDTO savedFacturaDTO = facturaMapper.toDto(facturaSave);
+
+            return savedFacturaDTO;
+
+        } catch (Exception e) {
+            throw new ServiceException(e);
+        }
     }
+
 
     @Override
     public FacturaDTO update(FacturaDTO facturaDTO) throws ServiceException {
@@ -40,8 +74,14 @@ public class FacturaServiceImpl implements FacturaService {
     }
 
     @Override
+    //   Metodo generado por el GenericService
     public void delete(Integer id) throws ServiceException {
-
+        try{
+//            llamamos al repository y  colocamos la Query que hace el borrado logico en la BD
+            facturaRepository.deleteCustom(id);
+        }catch (Exception  e) {
+            throw new ServiceException(e);
+        }
     }
 
     @Override
