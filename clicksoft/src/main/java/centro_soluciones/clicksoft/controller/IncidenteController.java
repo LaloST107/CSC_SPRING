1f088d9101543e660f39ea14033f885897749eef
Daniel-Fernandez-CS <clicksoftdaniel@gmail.com>
2024-02-15 12:16:27 -0600

    Cross origin

--- clicksoft/src/main/java/centro_soluciones/clicksoft/controller/IncidenteController.java
+++ clicksoft/src/main/java/centro_soluciones/clicksoft/controller/IncidenteController.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/incidentes")
+@CrossOrigin("http://localhost:4200")
 public class IncidenteController {
 
     private final IncidenteService incidenteService;
