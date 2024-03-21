93cf724d69ad633b8bd39cba5b1701be1ce56079
Daniel-Fernandez-CS <clicksoftdaniel@gmail.com>
2024-03-21 16:34:39 -0600

    Actualizacion de controllers para subir y mostrar imagenes desde el backend

--- clicksoft/src/main/java/centro_soluciones/clicksoft/controller/IncidenteController.java
+++ clicksoft/src/main/java/centro_soluciones/clicksoft/controller/IncidenteController.java
@@ -3,11 +3,25 @@ package centro_soluciones.clicksoft.controller;
 import centro_soluciones.clicksoft.dto.IncidenteDTO;
 import centro_soluciones.clicksoft.service.IncidenteService;
 import centro_soluciones.clicksoft.service.exception.ServiceException;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -16,6 +30,8 @@ import java.util.Optional;
 public class IncidenteController {
 
     private final IncidenteService incidenteService;
+
+    private final String imageDirectory = "src/main/resources/static/images/";
 
     public IncidenteController(IncidenteService incidenteService) {
         this.incidenteService = incidenteService;
@@ -88,4 +104,44 @@ public class IncidenteController {
         }
     }
 
+    @PostMapping("/upload-image")
+    public ResponseEntity<Map<String, String>> uploadImage(@RequestParam("imagen") MultipartFile imagen) {
+        if (imagen.isEmpty()) {
+            return ResponseEntity.badRequest().body(Collections.singletonMap("error", "La imagen está vacía"));
+        }
+
+        try {
+            // Guardar la imagen en una carpeta en el backend
+            String uploadsDir = "src/main/resources/static/images"; // Ruta donde se guarda la imagen
+            String fileName = imagen.getOriginalFilename();
+            String filePath = uploadsDir + "/" + fileName;
+            byte[] bytes = imagen.getBytes();
+            Path path = Paths.get(filePath);
+            Files.write(path, bytes);
+
+            return ResponseEntity.ok().body(Collections.singletonMap("message", "Imagen cargada exitosamente"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Collections.singletonMap("error", "Error al cargar la imagen"));
+        }
+    }
+
+    @GetMapping("/images/{imageName:.+}")
+    public ResponseEntity<Resource> serveImage(@PathVariable String imageName) {
+        try {
+            Path imagePath = Paths.get(imageDirectory).resolve(imageName);
+            Resource resource = new UrlResource(imagePath.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_JPEG) // o MediaType.IMAGE_PNG según el tipo de imagen
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + imageName + "\"")
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (MalformedURLException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
